a6a3c6791103ca4589d3de030ea226b60236f53d
nicolasMartinezCampos <nicolas.martinez23@gmail.com>
2017-10-19 17:30:32 +0200

    adding routes

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 var express = require('express'),
     app = express(),
-    path = require('path');
+    path = require('path'),
+    adminRouter = express.Router();
 
 //send our index.html file to the user for the home page
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+// admin main page. the dashboard (http://localhost:1337/admin)
+adminRouter.get('/', function(req, res) {
+    res.send('I am the dashboard!');
+    });
+
+//users page
+adminRouter.get('/users', function(req, res){
+    res.send('I show all the users');
+});
+
+//posts page
+adminRouter.get('/posts', function(req, res){
+    res.send('I show all the posts');
+});
+
+//apply the routes to our application
+app.use('/admin', adminRouter);
 
 // start the server
 app.listen(1337);
