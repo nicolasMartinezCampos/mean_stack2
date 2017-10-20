b22b9644a2611942d83c021a0670b26ad366b992
nicolasMartinezCampos <nicolas.martinez23@gmail.com>
2017-10-20 16:38:42 +0200

    ..

--- server.js
+++ server.js
@@ -31,6 +31,24 @@ adminRouter.get('/posts', function(req, res){
     res.send('I show all the posts');
 });
 
+//route middleware to validate :name
+adminRouter.param('name', function(req, res, next, name){
+    //do validation on name here
+    //...validation
+    //log something so we know its working
+    console.log('doing name validation on ' + name);
+
+    //one validation is done save the new item in the req
+    req.name = name;
+    //go to the next thing
+    next();
+});
+
+//route with parameters(http://localhost:1337/admin/hello/:name)
+adminRouter.get('/hello/:name', function(req, res){
+    res.send('Hello ' + req.name + '!');
+});
+
 //apply the routes to our application
 app.use('/admin', adminRouter);
 
